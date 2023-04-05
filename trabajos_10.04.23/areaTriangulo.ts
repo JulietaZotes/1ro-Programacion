2e491a1cf72b16f36d38c7b90d2f5eabd300c638
julietazotes <julietaazotes@gmail.com>
2023-04-05 17:35:50 -0300

    trabajos 10/04

---
+++ trabajos_10.04.23/areaTriangulo.ts
@@ -0,0 +1,25 @@
+
+import * as rls from "readline-sync";
+
+
+let area = 0
+
+function calcularAreaTriangulo (base: number, altura: number){
+    area = (base * altura)/2
+    return area;
+} 
+
+/*console.log(calcularAreaTriangulo(2,4))
+
+let areaPar1 = (calcularAreaTriangulo(1,2))
+
+console.log(areaPar1)*/
+
+/*let base = 0
+let altura = 0
+
+while(base <= 7 && altura <= 14){
+    base = rls.questionInt("ingrese base: ");
+    altura = rls.questionInt("ingrese la altura");
+   }*/
+   
